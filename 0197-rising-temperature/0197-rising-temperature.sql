68b1176fa5943ba59e07504dd7c852c02dd3e6fd
PatrykWenz-4 <patryk.wenz@gmail.com>
2024-10-27 22:45:25 +0100

    Time: 568 ms (35.45%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -2,7 +2,7 @@
 
 SELECT today.id
 FROM Weather yesterday 
-CROSS JOIN Weather today
+JOIN Weather today
 
 WHERE DATEDIFF(today.recordDate,yesterday.recordDate)=1
     AND today.temperature > yesterday.temperature;
