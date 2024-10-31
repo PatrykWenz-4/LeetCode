0527d1f965fb7dac5812bc92fefec8c740ddbfea
Patryk Wenz <patryk.wenz@gmail.com>
2024-10-31 18:27:43 +0100

    Time: 2090 ms (32%), Space: 110 MB (19.73%) - LeetHub

--- 2554-minimum-total-distance-traveled/2554-minimum-total-distance-traveled.py
+++ 2554-minimum-total-distance-traveled/2554-minimum-total-distance-traveled.py
@@ -1,17 +1,34 @@
 class Solution:
-    def minimumTotalDistance(self, robot: List[int], factory: List[List[int]]) -> int:
+    def minimumTotalDistance(
+        self, robot: List[int], factory: List[List[int]]
+    ) -> int:
         robot.sort()
-        factory.sort()
-        m, n = len(robot), len(factory)
-        dp = [[0]*(n+1) for _ in range(m+1)] 
-        for i in range(m): dp[i][-1] = inf 
-        for j in range(n-1, -1, -1): 
-            prefix = 0 
-            qq = deque([(m, 0)])
-            for i in range(m-1, -1, -1): 
-                prefix += abs(robot[i] - factory[j][0])
-                if qq[0][0] > i+factory[j][1]: qq.popleft()
-                while qq and qq[-1][1] >= dp[i][j+1] - prefix: qq.pop()
-                qq.append((i, dp[i][j+1] - prefix))
-                dp[i][j] = qq[0][1] + prefix
-        return dp[0][0]
+        factory.sort(key=lambda x: x[0])
+        factory_positions = []
+        for f in factory:
+            factory_positions.extend([f[0]] * f[1])
+        robot_count = len(robot)
+        factory_count = len(factory_positions)
+
+        dp = [[None] * (factory_count + 1) for _ in range(robot_count + 1)]
+
+        def _calculate_min_distance(robot_idx: int, factory_idx: int) -> int:
+            if dp[robot_idx][factory_idx] is not None:
+                return dp[robot_idx][factory_idx]
+            if robot_idx == robot_count:
+                dp[robot_idx][factory_idx] = 0
+                return 0
+            if factory_idx == factory_count:
+                dp[robot_idx][factory_idx] = int(1e12)
+                return int(1e12)
+
+            assign = abs(
+                robot[robot_idx] - factory_positions[factory_idx]
+            ) + _calculate_min_distance(robot_idx + 1, factory_idx + 1)
+
+            skip = _calculate_min_distance(robot_idx, factory_idx + 1)
+
+            dp[robot_idx][factory_idx] = min(assign, skip)
+            return dp[robot_idx][factory_idx]
+
+        return _calculate_min_distance(0, 0)
