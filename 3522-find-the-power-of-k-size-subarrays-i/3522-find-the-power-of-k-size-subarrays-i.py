54220a1ce0f9cfd58b333c5812197a22428fa84b
Patryk Wenz <patryk.wenz@gmail.com>
2024-11-16 16:33:39 +0100

    Time: 51 ms (25.53%), Space: 16.9 MB (5.94%) - LeetHub

--- 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.py
+++ 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.py
@@ -1,20 +1,19 @@
 class Solution:
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
         results = []
-
-        res = []
-        l = 0
-        consec_cnt = 1
-        for r in range(len(nums)):
-            if r > 0 and nums[r - 1] + 1 == nums[r]:
-                consec_cnt += 1
-
-            if r - l + 1 > k:
-                if nums[l] + 1 == nums[l + 1]:
-                    consec_cnt -= 1
-                l += 1
-
-            if r - l + 1 == k:
-                res.append(nums[r] if consec_cnt == k else -1)
-        return res
         
+        for i in range(len(nums) - k + 1):
+            current_window = nums[i:i + k]
+
+            is_consecutive = True
+            for j in range(k - 1):
+                if current_window[j + 1] - current_window[j] != 1:
+                    is_consecutive = False
+                    break
+
+            if is_consecutive:
+                results.append(max(current_window))
+            else:
+                results.append(-1)
+
+        return results
