f45574cced357a49be5f3fd4212aab391de02d65
Patryk Wenz <patryk.wenz@gmail.com>
2025-02-16 16:41:13 +0100

    Time: 40 ms (97.36%), Space: 48.8 MB (62.74%) - LeetHub

---
+++ 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    
+    const newArray = [];
+    arr.forEach((element,index) => {
+        newArray[index] = fn(element,index);
+    });
+    return newArray;
+
+
+};
