4c2e530065da9c467cb54483bf330a93318e83d4
Patryk Wenz <patryk.wenz@gmail.com>
2025-02-16 13:04:21 +0100

    Time: 52 ms (88.83%), Space: 51.4 MB (83.63%) - LeetHub

---
+++ 2789-counter-ii/2789-counter-ii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let number = init;
+
+    function increment(){
+        return number += 1;
+    }
+    function reset(){
+        return number = init;
+    }
+    function decrement(){
+        return number -= 1;
+    }
+
+  return { increment, decrement, reset };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
