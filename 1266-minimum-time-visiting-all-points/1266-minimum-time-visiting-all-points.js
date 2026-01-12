ea6b3e6aa59096170e82ea9070c2f91b72f3a68b
Patryk Wenz <patryk.wenz@gmail.com>
2026-01-12 19:48:24 +0100

    Time: 2 ms (26.88%), Space: 55.7 MB (50.54%) - LeetHub

---
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var minTimeToVisitAllPoints = function(points) {
+    var dist = 0;
+    for(let i = 0; i < points.length - 1; i++){
+            const [x1, y1] = points[i];
+            const [x2, y2] = points[i + 1];
+            const dx = Math.abs(x2 - x1);
+            const dy = Math.abs(y2 - y1);
+
+            dist += Math.max(dx, dy);
+    }
+    return dist;
+};
