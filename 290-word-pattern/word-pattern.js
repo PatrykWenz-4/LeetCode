41c86bf1ddb43f03a50336d53cded3bb6facdff7
Patryk Wenz <patryk.wenz@gmail.com>
2026-04-12 19:19:58 +0200

    Time: 5 ms (4.37%) | Memory: 53.6 MB (56.01%) - LeetSync

---
+++ 290-word-pattern/word-pattern.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, s) {
+    const words = s.split(' ');
+    
+    if (pattern.length !== words.length) {
+        return false;
+    }
+
+    const map = new Map();
+
+    for (let i = 0; i < pattern.length; i++) {
+        const charKey = `char_${pattern[i]}`;
+        const wordKey = `word_${words[i]}`;
+
+        if (map.get(charKey) !== map.get(wordKey)) {
+            return false;
+        }
+
+        map.set(charKey, i);
+        map.set(wordKey, i);
+
+        console.log(charKey)
+    }
+
+    return true;
+};
