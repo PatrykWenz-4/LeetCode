a26012095470bf2e00a4131a558e7c55a82efa02
Patryk Wenz <patryk.wenz@gmail.com>
2026-04-14 19:26:43 +0200

    Time: 272 ms (6.09%), Space: 64.3 MB (5.36%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindrome = function(s) {
+    
+    let map = new Map();
+
+    for(let i =0; i<s.length; i++){
+        map.set(s[i], (map.get(s[i])||0) + 1);
+        console.log(map);
+    }
+
+    let sum = 0;
+    let odd = 0;
+    for(let count of map.values()){
+        if(count %2 === 0 ){
+            sum += count;
+        }else{
+            sum += count -1;
+            odd = 1;
+        }
+    }
+
+    return  sum+odd;
+
+};
