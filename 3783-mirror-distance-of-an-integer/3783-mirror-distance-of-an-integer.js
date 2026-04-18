7305224e3df3f468ecc4316b791a12f4daf3ccae
Patryk Wenz <patryk.wenz@gmail.com>
2026-04-18 21:10:16 +0200

    Time: 0 ms (100%), Space: 55.7 MB (54.96%) - LeetHub

---
+++ 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var reverse = function (n) {
+    let res = 0;
+    while (n > 0) {
+        res = res * 10 + (n % 10);
+        n = Math.floor(n / 10);
+    }
+    return res;
+};
+
+var mirrorDistance = function (n) {
+    return Math.abs(n - reverse(n));
+};
