784b54f1debd04de729cb717a73809feb1fbc938
Patryk Wenz <patryk.wenz@gmail.com>
2026-05-03 18:13:49 +0200

    Time: 0 ms (100%), Space: 53.7 MB (57.56%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.js
+++ 0796-rotate-string/0796-rotate-string.js
@@ -4,21 +4,5 @@
  * @return {boolean}
  */
 var rotateString = function(s, goal) {
-    
-    if (s.length !== goal.length) {
-        return false;
-    }
-
-    let length = s.length;
-
-    for (let i = 0; i < length; i++) {
-        s = s.slice(1) + s[0];
-        if (s === goal) {
-            return true;
-        }
-    }
-    
-    return false;
-
-
+    return s.length === goal.length && (s + s).includes(goal);
 };
